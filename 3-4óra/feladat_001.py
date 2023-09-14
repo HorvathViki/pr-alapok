dc242d377f124caf77923d4a48eb55dc8571d36f
HorvathViki <125078623+HorvathViki@users.noreply.github.com>
2023-09-14 10:28:18 +0200

    Add files via upload

---
+++ 3-4óra/feladat_001.py
@@ -0,0 +1,20 @@
+#feladat_001
+"""
+Kérjük be a billenytyüzetről a nevünket és irássuk ki a képeernyöre!
+A billenytyüzetröl mindig szöveget (string-et) olvasunk  be!
+type(változó)
+"""
+
+'''
+nev = input("Kérek egy nevet: ")
+print(f"A megadott név a következő: {nev}")
+'''
+
+vnev = input("Kérek egy vezetéknevet: ")
+knev = input("Kérek egy keresztnevet: ")
+print(f"A megadott név a következő: {vnev} {knev}")
+
+
+
+print(f"A változó típusa: {type(vnev)}")
+print(f"A változó típusa: {type(knev)}")
