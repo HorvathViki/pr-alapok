2792fb6b33979be56ab1c3c8dd02b07db3115bf8
HorvathViki <125078623+HorvathViki@users.noreply.github.com>
2023-09-28 10:04:37 +0200

    Add files via upload

---
+++ hazi/hazi1.py
@@ -0,0 +1,8 @@
+#hazifeladat_1
+nap = input("Jó napod volt?")
+if nap == "igen":
+    print(f"Örülök neki")
+elif nap == "nem":
+    print(f"Remélem holnap jobb lesz")
+else:
+    print(f"Sajnos ezt nem értem")
---
+++ hazi/hazi2.py
@@ -0,0 +1,8 @@
+#hazifeladat_2
+nap = input("Jó napod volt?")
+if nap == "igen":
+    print("Örülök neki")
+elif nap == "nem":
+    print("Remélem holnap jobb lesz")
+else:
+    print("Sajnos ezt nem értem")
