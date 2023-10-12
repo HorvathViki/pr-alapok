936c7c5dbf4c787fda1951d129391fa540bc3d19
HorvathViki <125078623+HorvathViki@users.noreply.github.com>
2023-10-12 10:32:03 +0200

    Add files via upload

--- feladatok/feladat_008.py
+++ feladatok/feladat_008.py
@@ -1,13 +1,21 @@
 #feladat_008
 sebesség = input("Add meg a sebességet:")
-sebesség=int(sebesség)
+sebesség = int(sebesség)
+
 if sebesség == 0:
     print(f'Megálltál')
-    if sebesség < =50:
-    print(f'Büntetés 15000FT')
-    if sebesség < =65:
-    print(f'Büntetés 30000FT')
-    elif sebesség < =80:
-    print(f'Büntetés 60000')
-    elif sebesség < =100:
-    print(f'Elveszik a jogositványt')
+
+if sebesség  == 50:
+    print(f"Büntetés 15000FT")
+          
+elif sebesség == 65:
+    print(f"Büntetés 30000FT")
+
+elif sebesség == 80:
+    print(f"Büntetés 60000")
+
+elif sebesség == 100:
+    print(f"Elveszik a jogositványt")
+     
+else :
+    print(f"Nem érvenyes")
