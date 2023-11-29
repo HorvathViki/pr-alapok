9ca4bc66a29fcb65f2e46cc11d48d01b08f1192b
HorvathViki <125078623+HorvathViki@users.noreply.github.com>
2023-11-29 20:49:44 +0100

    Add files via upload

---
+++ feladatok/hazifeladat_016.py
@@ -0,0 +1,8 @@
+#többszöri be kérés
+folytatja = True
+while folytatja:
+      print('a kedvenc emberem laura!')
+      valasz = input('Mondjam még egyszer? (i/n): ')
+      if valasz == 'n':
+          folytatja = False
+print('>> Program vége! <<')      
---
+++ feladatok/hazifeladat_017.py
@@ -0,0 +1,8 @@
+#paros szam bekeres
+szam = int(input('Adj meg egy számot ami páros! '))
+
+while not szam %2 == 0 :
+      szam = int(input('Helytelen érték! Adj meg egy számot ami páros! '))
+  
+print('Rendben!')     
+  
